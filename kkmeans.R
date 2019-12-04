d46dfd24fac441c3ca11388f1738962fab071865
Marcel Porta Vallés <marcelpv96@gmail.com>
2019-12-04 19:00:21 +0100

    kkmeans done

---
+++ kkmeans.R
@@ -0,0 +1,52 @@
+# This is the difference between the new kernel approach and normal approach. In kernels you can not say
+get_distance <- function(K, n, k, I){
+  dist <- K[n,n] - 2*I[,k] %*% K[,n] / sum(I[,k])
+  numerator <- sum(I[,k] * K %*% I[,k]) 
+  denominator <- (I[,k]%*%I[,k])**2
+  dist <- dist + numerator/denominator
+  return(dist)
+}
+
+# Function own kkmeans
+kkmeans_own <- function(K, c, clusters, N){
+  new.clusters <- matrix(nrow=N, ncol=c) # list of new clusters 
+  start_time <- Sys.time()
+  for (n in 1:N){
+    distances <- c()
+    for (i in 1:c){ # For each cluster obtain which distance are this point 'n' to him.
+      distances <- c(distances, get_distance(K, n, i, clusters))
+    }
+    new.clusters[n,] <- as.numeric(distances == min(distances))
+  }
+  end_time <-Sys.time()
+  print(end_time-start_time)
+  if(identical(new.clusters, clusters)){
+    return(new.clusters)
+  }else{
+    kkmeans_own(K, c, new.clusters, N)
+  }
+}
+
+# Function one hot encoding cluster
+ohencoding_cluster <- function(clusters, num_clusters){
+  dummy_variables <- matrix(nrow=length(clusters), ncol = num_clusters)
+  for (k in 1:num_clusters){
+    dummy_variables[,k] <- as.numeric(clusters==k)
+  }
+  return(dummy_variables)
+}
+
+# Function obtain labels from onehot encoding
+cluster_labels <- function(dummy_variables, num_clusters){
+  clusters <- c()
+  for(i in 1:nrow(dummy_variables)){
+    clusters <- c(clusters, which(dummy_variables[i,] == 1))
+  }
+  return(clusters)
+}
+
+
+
+
+
+
---
+++ main.R
@@ -0,0 +1,36 @@
+library(kernlab)
+source("kkmeans.R")
+
+# Load data
+ring_data <- read.csv("ring_dataset.csv")
+ring.matrix <- as.matrix(ring_data)[,2:3]
+
+# Plot the original data
+plot(ring.matrix, col=as.factor(ring_data$label))
+
+
+# Perform kmeans, and plot result
+cluster.kmeans <- kmeans(ring.matrix, 2)
+plot(ring.matrix, col=as.factor(cluster.kmeans$cluster))
+
+
+# Perform kkmeans, and plot result
+cluster.kkmeans <- kkmeans(ring.matrix, 2, kernel='rbfdot')
+plot(ring.matrix, col=as.factor(cluster.kkmeans@.Data))
+
+
+# Perform own kkmeans, and plot result
+ini.clusters <- cluster.kmeans$cluster
+ini.ohencoding <- ohencoding_cluster(ini.clusters, 2)
+K <- kernelMatrix(rbfdot(), ring.matrix, y = NULL)
+
+cluster.kkmenas_own <- kkmeans_own(K, 2, ini.ohencoding, nrow(K))
+
+plot(ring.matrix, col=as.factor(ini.clusters))
+plot(ring.matrix, col=as.factor(cluster_labels(cluster.kkmenas_own, 2)))
+
+
+
+
+
+
